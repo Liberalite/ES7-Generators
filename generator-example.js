1fae20e3ab32463413ceaa9999468724d5a97f0d
Octavian Lojnita <octales@yahoo.com>
2017-08-06 04:45:33 +0300

    Update generator-example.js

--- generator-example.js
+++ generator-example.js
@@ -1,3 +1,7 @@
+/**
+ * @public Generator function chaining example
+ */
+
 function* anotherGenerator(i) {
   yield i + 1;
   yield i + 2;
@@ -11,6 +15,10 @@ function* generator(i) {
 }
 
 var gen = generator(10);
+
+/**
+ * @method METHOD 1 - Ensures function C finishes before calling function D
+ */
 
 console.log(gen.next().value); // 10
 console.log(gen.next().value); // 11
